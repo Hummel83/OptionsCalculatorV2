89326e33995f2981f37737698137cc65f0d19904
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2020-09-10 22:52:54 +0200

    BlackScholes Class

---
+++ OptionsCalculatorV2/BlackScholes/BlackScholes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OptionsCalculatorV2.BlackScholes
+{
+    public class BlackScholes
+    {
+        public double underlyingPrice { get; }
+        public double strikePrice { get; }
+        public double DTE { get; }
+        
+        public double YTE => this.DTE / 365;
+        public double riskFreeRate { get; }
+        public double historicalVolatility { get; }
+        public double dividendYield { get; }
+
+        public BlackScholes(double underlyingPrice, double strikePrice, double dte, double riskFreeRate, double historicalVolatility, double dividendYield)
+        {
+            this.underlyingPrice = underlyingPrice;
+            this.strikePrice = strikePrice;
+            this.DTE = dte;
+            this.riskFreeRate = riskFreeRate / 100;
+            this.historicalVolatility = historicalVolatility / 100;
+            this.dividendYield = dividendYield / 100;
+        }
+        
