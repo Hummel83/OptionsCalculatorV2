99d20c4e1810e6cd387f31a915d0fade93c4e892
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2020-09-10 22:53:15 +0200

    main class

---
+++ OptionsCalculatorV2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+using Microsoft.Office.Interop.Excel;
+using OptionsCalculatorV2.BlackScholes;
+using Application = System.Windows.Forms.Application;
+
+namespace OptionsCalculatorV2
+{
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
